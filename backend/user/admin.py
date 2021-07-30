f9f7a83e5615a90064ad9972d4697bf29616636f
ümit <umitde296@gmail.com>
2021-07-30 22:53:31 +0300

    added users to admin

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from user.models import User, UserProfile
+
+
+class UserProfileInline(admin.TabularInline):
+    model = UserProfile
+    extra = 0
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    inlines = [UserProfileInline]
+    search_fields = ("username",)
+    exclude = ("password",)
