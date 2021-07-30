ce2f4ce5b62c6825129c4e8e335db0e381cb8b44
ümit <umitde296@gmail.com>
2021-07-30 23:05:35 +0300

    added new utils for files

--- backend/user/utils/__init__.py
+++ backend/user/utils/__init__.py
@@ -1,8 +1,24 @@
-import uuid
-from os.path import splitext
+from typing import TYPE_CHECKING
+
+from core.utils.files import generate_new_filename
+
+if TYPE_CHECKING:
+    from user.models import UserProfile, User
+
 
 def create_username_with_mail():
     pass
 
 
-def upload_to_user_directory(user, filename):
+def upload_to_user_directory(
+    user_profile: "UserProfile",
+    filename: str,
+) -> str:
+    user = user_profile.user
+    return _upload_to_user_directory(user, filename)
+
+
+def _upload_to_user_directory(user: "User", filename: str) -> str:
+    generated_filename = generate_new_filename(filename)
+    path = f"users/{user.token}/{generated_filename}"
+    return path
