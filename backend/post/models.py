6badf9ab6fd4abb4e8ac8774123e8d98da3e4a4e
umtdemr <umitde692@gmail.com>
2021-08-02 15:27:01 +0300

    added basic post models

--- backend/post/models.py
+++ backend/post/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+
+from post import PostStatus, PostVisibility
+from core.abstract_models import TimeInfoModel
+
+
+class Post(TimeInfoModel):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             related_name="posts",
+                             on_delete=models.CASCADE)
+    uuid = models.UUIDField(default=uuid.uuid4,
+                            editable=False,
+                            unique=True,
+                            db_index=True)
+    status = models.CharField(
+        max_length=12,
+        choices=PostStatus.CHOICES,
+        default=PostStatus.DRAFT,
+    )
+    visibility = models.CharField(
+        max_length=12,
+        choices=PostVisibility.CHOICES,
+        default=PostVisibility.VISIBLE,
+    )
+    text = models.TextField()
+    is_edited = models.BooleanField(default=False)
