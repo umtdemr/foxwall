b0cb7ddb5e1bf194efefcffecd28682eca5abb61
umtdemr <umitde692@gmail.com>
2021-08-03 09:29:31 +0300

    like added to adming

--- backend/like/admin.py
+++ backend/like/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from like.models import Like
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    search_fields = (
+        "user_id",
+        "user__name",
+    )
+    list_display = ["user", "post", "created_at"]
