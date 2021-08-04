73defa4e5941b977343b17f14582ad202be68343
umtdemr <umitde692@gmail.com>
2021-08-04 13:12:34 +0300

    started to testing

--- backend/tests/fixtures.py
+++ backend/tests/fixtures.py
@@ -1,1 +1,42 @@
+from io import BytesIO
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.conf import settings
+
+from PIL import Image
+import pytest
+
 from user.models import User, UserProfile
+
+
+@pytest.fixture
+def image():
+    img_data = BytesIO()
+    image = Image.new("RGB", size=(1, 1))
+    image.save(img_data, format="JPEG")
+    return SimpleUploadedFile("product.jpg", img_data.getvalue())
+
+
+@pytest.fixture
+def media_root(tmpdir, settings):
+    settings.MEDIA_ROOT = str(tmpdir.mkdir("media"))
+
+
+@pytest.fixture
+def valid_user(db):
+    user = User.objects.create(
+        username="mediumgoal",
+        email="deneme@w.com",
+        password="password"
+    )
+    return user
+
+
+@pytest.fixture
+def valid_user_profile(db, valid_user, image, media_root):
+    return UserProfile.objects.create(
+        user=valid_user,
+        avatar=image,
+        cover=image,
+        bio="some text"
+    )
---
+++ backend/user/test/test_models.py
@@ -0,0 +1,26 @@
+import os
+import pytest
+
+from ..models import User, UserProfile
+
+
+def test_user_can_create(valid_user):
+    count = User.objects.count()
+    assert count == 1
+
+
+def test_user_profile_can_create(valid_user_profile):
+    count = UserProfile.objects.count()
+    assert count == 1
+
+
+def test_user_profile_media_path(tmpdir, valid_user_profile):
+    user_profile = valid_user_profile
+    cover_file_name = os.path.basename(user_profile.cover.name)
+    avatar_file_name = os.path.basename(user_profile.avatar.name)
+    t_cov = f"{tmpdir}/media/users/{user_profile.user.token}/{cover_file_name}"
+    t_av = f"{tmpdir}/media/users/{user_profile.user.token}/{avatar_file_name}"
+    assert os.path.exists(t_cov)
+    assert os.path.exists(t_av)
+    assert user_profile.cover.path == t_cov
+    assert user_profile.avatar.path == t_av
