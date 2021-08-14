87802f9c93131e2ec8a7584d7baaa8dd5796a4b2
umtdemr <umitde692@gmail.com>
2021-08-14 18:50:28 +0300

    added is_throw

--- backend/user/test/views/test_auth.py
+++ backend/user/test/views/test_auth.py
@@ -35,3 +35,52 @@ def test_login(
 
         assert response.data.get("email") == valid_user2.email
         assert response.data.get("token") == valid_user2.token
+
+
+@pytest.mark.parametrize(
+    "email, username, password, name, is_cover, is_error, is_throw",
+    [
+        ("test@test.com", "tst", "emesifre", "Ümit Demir", True, False, False),
+        ("test@test.com", "tst", "desifre", "Ümit Demir", False, False, False),
+        (None, "tst", "denemesifre", "Ümit Demir", False, True, True),
+        ("test@test.com", "tst", "password", "Ümit Demir", True, True, False),
+        ("test@test.com", "tst", "=9smksd+-", "Ümit<Demir", True, True, False),
+        ("test@test.com", "tst", None, "Ümit Demir", True, True, True),
+        ("test@test.com", "tst", "x13j", "Ümit Demir", True, False, False),
+    ]
+)
+def test_register(
+    email,
+    username,
+    password,
+    name,
+    is_cover,
+    is_error,
+    is_throw,
+    image,
+    media_root,
+    api_client,
+    valid_user,
+):
+    register_data = {
+        "email": email,
+        "username": username,
+        "password": password,
+        "name": name,
+    }
+
+    if is_cover:
+        register_data["cover"] = image
+    if is_throw:
+        with pytest.raises(TypeError):
+            response = api_client().post("/user/register/", register_data)
+    else:
+        response = api_client().post("/user/register/", register_data)
+        print(response)
+
+        if is_error:
+            assert response.status_code == 400
+
+        print(response)
+        print(response.status_code)
+        print(response.data)
