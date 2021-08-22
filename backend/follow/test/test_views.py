9683c7b8cc478c93cbb34a80621469662250f232
umtdemr <umitde692@gmail.com>
2021-08-22 14:59:19 +0300

    added testes for follow requests

--- backend/follow/test/test_views.py
+++ backend/follow/test/test_views.py
@@ -1,19 +1,53 @@
+import pytest
+from typing import TYPE_CHECKING
+
 from tests.helpers import login_with_client
 
 
+if TYPE_CHECKING:
+    from _pytest.fixtures import FixtureRequest
+
+
+@pytest.mark.parametrize(
+    "creator, target, status_code",
+    [
+        (
+            "valid_user",
+            "valid_user2",
+            201
+        ),
+        (
+            "valid_user2",
+            "valid_user2",
+            400
+        ),
+        (
+            "valid_user2",
+            "s",
+            400
+        ),
+    ]
+)
 def test_user_can_follow_request(
-    valid_user,
-    valid_user2,
+    creator,
+    target,
+    status_code,
     api_client,
+    request: "FixtureRequest",
 ):
+    creator = request.getfixturevalue(creator)
+    try:
+        target = request.getfixturevalue(target)
+        target_username = target.username
+    except Exception:
+        target_username = "not_exist_username"
     client = login_with_client(
         api_client(),
-        valid_user
+        creator
     )
 
     response = client.post("/follow/request/", {
-        "username": valid_user2.username,
+        "username": target_username,
     })
 
-    assert response.status_code == 201
-    assert response.data.get("message") == "created"
+    assert response.status_code == status_code
