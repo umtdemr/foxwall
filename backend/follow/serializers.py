611f5ab50f5bd105761928ca22a4a593c8c1f107
umtdemr <umitde692@gmail.com>
2021-08-22 14:12:05 +0300

    validation added to serializer

--- backend/follow/serializers.py
+++ backend/follow/serializers.py
@@ -2,13 +2,17 @@ from django.conf import settings
 
 from rest_framework import serializers
 
-from core.validators import username_special_character_validator
+from core.validators import (
+    username_special_character_validator,
+    user_username_exists,
+)
 
 
 class RequestWithUsernameSerializer(serializers.Serializer):
     username = serializers.CharField(
         max_length=settings.MAX_USERNAME_LENGTH,
         validators=[
-            username_special_character_validator
+            username_special_character_validator,
+            user_username_exists
         ]
     )
