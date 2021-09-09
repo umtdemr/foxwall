92dcd404d069ec8ba8074a53faf798db08fcb799
umtdemr <umitde692@gmail.com>
2021-09-09 11:49:12 +0300

    added test for follows api view

--- backend/user/test/views/test_followers.py
+++ backend/user/test/views/test_followers.py
@@ -1,15 +1,17 @@
 from tests.helpers import login_with_client
 
 
-def test_followers_api_view(
-    follow_obj,
-    api_client
-):
-    client = login_with_client(
-        api_client(),
-        follow_obj.followed_user.token
-    )
+def test_followers_api_view(follow_obj, api_client):
+    client = login_with_client(api_client(), follow_obj.followed_user.token)
 
     response = client.get("/user/followers/")
+
+    assert response.status_code == 200
+
+
+def test_follows_api_view(follow_obj, api_client):
+    client = login_with_client(api_client(), follow_obj.user.token)
+
+    response = client.get("/user/follows/")
 
     assert response.status_code == 200
