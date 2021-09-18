6b44bbe9d4ee13d60da099b56ad3308881daf9d2
umtdemr <umitde692@gmail.com>
2021-09-18 18:26:54 +0300

    added like create test

---
+++ backend/like/test/test_views.py
@@ -0,0 +1,23 @@
+import pytest
+
+from tests.helpers import login_with_client
+from like.models import Like
+
+
+pytestmark = pytest.mark.django_db
+
+
+def test_like_post(
+    api_client,
+    valid_user_profile,
+    post_obj
+):
+    client = login_with_client(
+        api_client(),
+        valid_user_profile.user.token
+    )
+
+    response = client.post("/like/", {"uuid": post_obj.uuid})
+
+    assert response.status_code == 201
+    assert Like.objects.count() == 1
