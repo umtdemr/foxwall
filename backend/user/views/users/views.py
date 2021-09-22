3204b4714a16e3e2b9d8791df34bf07cb1373995
umtdemr <umitde692@gmail.com>
2021-09-22 14:47:49 +0300

    seperated serializer fields

--- backend/user/views/users/views.py
+++ backend/user/views/users/views.py
@@ -3,6 +3,8 @@ from typing import TYPE_CHECKING
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from user.serializers import RequestWithUsernameSerializer
+
 
 if TYPE_CHECKING:
     from django.http import HttpRequest
@@ -11,7 +13,10 @@ if TYPE_CHECKING:
 class GetUserAPIView(APIView):
 
     def get(self, request: "HttpRequest", username: str):
-        print(request)
-        print(username)
+
+        username_validation_serializer = RequestWithUsernameSerializer(
+            data={"username": username}
+        )
+        username_validation_serializer.is_valid(raise_exception=True)
 
         return Response({"message": "Started!"})
