d1b32e97a503e35e214c4ad32d810a0e8b67b361
umtdemr <umitde692@gmail.com>
2021-09-23 20:22:56 +0300

    test configured for new fields

--- backend/user/test/views/test_users.py
+++ backend/user/test/views/test_users.py
@@ -4,15 +4,19 @@ from tests.helpers import login_with_client
 def test_get_user_api_view(
     api_client,
     follow_obj,
-    valid_user,
-    valid_user2
+    follow_obj2,
 ):
 
     client = login_with_client(
         api_client(),
-        valid_user2.token,
+        follow_obj.user.token,
     )
 
-    response = client.get(f"/user/profile/{valid_user2.username}")
+    response = client.get(
+        f"/user/profile/{follow_obj.followed_user.username}/"
+    )
 
     assert response.status_code == 200
+    print(response.data.get("user"))
+    assert response.data.get("user").get("is_following")
+    assert response.data.get("user").get("is_followed_me")
