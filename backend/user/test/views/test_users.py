726792feab11ad8fd57f6352e1d514e0330443e9
umtdemr <umitde692@gmail.com>
2021-09-26 10:21:04 +0300

    added get user's post test

--- backend/user/test/views/test_users.py
+++ backend/user/test/views/test_users.py
@@ -21,3 +21,20 @@ def test_get_user_api_view(
     assert response.data.get("user").get("is_followed_me") is True
     assert response.data.get("user").get("is_sent_follow_request") is False
     assert response.data.get("user").get("is_came_follow_request") is False
+
+
+def test_get_user_posts_api_view(
+    api_client,
+    follow_obj,
+    post_obj,
+):
+    client = login_with_client(
+        api_client(),
+        follow_obj.user.token
+    )
+
+    response = client.get(
+        f"/user/{follow_obj.followed_user.username}/posts/"
+    )
+
+    assert response.status_code == 200
