de3210f5c9603fab57a444351d4782015c69fb59
umtdemr <umitde692@gmail.com>
2022-02-06 01:09:13 +0300

    search performance improved

--- user/utils/search.py
+++ user/utils/search.py
@@ -5,8 +5,9 @@ from user.models import User
 
 def search_user(q: str):
     query = User.objects.filter(
-        Q(username__icontains=q) |
-        Q(email__icontains=q) |
-        Q(profile__name__icontains=q)
+        Q(username__icontains=q.lower()) |
+        Q(email__icontains=q.lower()) |
+        Q(profile__name__icontains=q.lower()) |
+        Q(profile__bio__icontains=q.lower())
     )
     return query, query.count()
